170181168b7bc40773ec5a94260b8fdbe7deb55c
Sazzads <sazzadtk@gmail.com>
2023-05-02 10:57:25 +0600

    create react app and install all dependency

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBz1d8BdXFGBiBk8f0PJKlsTPTpb25mbcg",
+    authDomain: "work-10-70bc2.firebaseapp.com",
+    projectId: "work-10-70bc2",
+    storageBucket: "work-10-70bc2.appspot.com",
+    messagingSenderId: "1090950431115",
+    appId: "1:1090950431115:web:fdf38506c885375ed8af8e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app; 
